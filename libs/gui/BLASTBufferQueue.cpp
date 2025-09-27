11c23e8d00f08d6eda71b3f30df7454f98b4e9d1
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-27 16:57:59 +0800

    adding scroll optimizer port hooks
    Change-Id: Id258e90ed1827cb4e0b844c2e7baa1e79bce1c67
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- libs/gui/BLASTBufferQueue.cpp
+++ libs/gui/BLASTBufferQueue.cpp
@@ -535,6 +535,7 @@ void BLASTBufferQueue::releaseBuffer(const ReleaseCallbackId& callbackId,
         return;
     }
     mNumAcquired--;
+    mQtiNumUndequeued++;
     BBQ_TRACE("frame=%" PRIu64, callbackId.framenumber);
     BQA_LOGV("released %s", callbackId.to_string().c_str());
     mBufferItemConsumer->releaseBuffer(it->second, releaseFence);
@@ -862,6 +863,7 @@ void BLASTBufferQueue::onFrameReplaced(const BufferItem& item) {
 void BLASTBufferQueue::onFrameDequeued(const uint64_t bufferId) {
     std::lock_guard _lock{mTimestampMutex};
     mDequeueTimestamps.emplace_or_replace(bufferId, systemTime());
+    mQtiNumUndequeued--;
 };
 
 void BLASTBufferQueue::onFrameCancelled(const uint64_t bufferId) {
